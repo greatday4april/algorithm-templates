61e29e3911b98ad3beee2fcec526898431ba4cdd
april <greatday4april@gmail.com>
2022-05-22 16:59:36 -0700

    do module properly

--- algo/lib/ordered_set.rb
+++ algo/lib/ordered_set.rb
@@ -1,33 +1,32 @@
 # implement a set that respects insertion order
+module Algo
+  class OrderedSet
+    def initialize
+      @hash = {}
+    end
 
+    def add(element)
+      @hash[element] = element
+    end
 
-class OrderedSet
-  include Algo
-  def initialize
-    @hash = {}
-  end
+    def delete(element)
+      @hash.delete(element)
+    end
 
-  def add(element)
-    @hash[element] = element
-  end
+    def include?(element)
+      @hash.key?(element)
+    end
 
-  def delete(element)
-    @hash.delete(element)
-  end
+    def shift
+      @hash.shift.first
+    end
 
-  def include?(element)
-    @hash.key?(element)
-  end
+    def first
+      @hash.first.first
+    end
 
-  def shift()
-    @hash.shift.first
-  end
-
-  def first()
-    @hash.first.first
-  end
-
-  def to_a
-    @hash.keys
+    def to_a
+      @hash.keys
+    end
   end
 end
